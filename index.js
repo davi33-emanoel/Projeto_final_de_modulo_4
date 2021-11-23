83e3ec54061d34d4df1d8d40d283ca38aee30141
davi33-emanoel <emanoeldavi348@gmail.com>
2021-11-23 15:00:24 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 const middleware = require('./controller/midlewares')
 const produtos = require('./controller/produtos_controllers')
 const produtos_database = require('./database/sqlite')
@@ -9,4 +9,4 @@ const rota = require('./controller/rotas')
 middleware(app)
 produtos(app,produtos_database)
 rota(app)
-app.listen(3000, console.log(`servidor rodando na porta 3000, link http://localhost:${port}/`))
+app.listen(PORT, console.log(`servidor rodando na porta 3000, link http://localhost:${port}/`))
